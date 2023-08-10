4a4ff195eda4f4b563b8da5572f1325263575969
Nicholas Dille <nicholas.dille@mailbox.org>
2023-08-10 18:56:52 +0200

    Wrap download of metadata

--- make/site.mk
+++ make/site.mk
@@ -1,5 +1,16 @@
 ALL_TOOLS_RAW := $(shell jq --raw-output '.tools[].name' metadata.json | xargs echo)
 
+metadata.json: \
+		$(HELPER)/var/lib/uniget/manifests/gojq.json \
+		$(HELPER)/var/lib/uniget/manifests/regclient.json \
+		; $(info $(M) Downloading metadata...)
+	@helper/usr/local/bin/regctl manifest get ghcr.io/uniget-org/tools/metadata:main --format raw-body \
+	| helper/usr/local/bin/jq --raw-output '.layers[0].digest' \
+	| xargs -I{} \
+		helper/usr/local/bin/regctl blob get ghcr.io/uniget-org/tools/metadata:main {} \
+	| tar -xz --to-stdout \
+	>metadata.json
+
 .PHONY:
 pages: \
 		$(addprefix site/content/tools/,$(addsuffix .md,$(ALL_TOOLS_RAW)))
