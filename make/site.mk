b1c0fb5a924ae9fd8a6f064c643ee245ede9c97b
seat1 <nicholas.dille@mailbox.org>
2024-10-27 19:00:30 +0100

    Fixed download of metadata

--- make/site.mk
+++ make/site.mk
@@ -4,7 +4,7 @@ metadata.json: \
 		$(HELPER)/var/lib/uniget/manifests/gojq.json \
 		$(HELPER)/var/lib/uniget/manifests/regclient.json \
 		; $(info $(M) Downloading metadata...)
-	@helper/usr/local/bin/regctl manifest get ghcr.io/uniget-org/tools/metadata:main --format raw-body \
+	@helper/usr/local/bin/regctl manifest get ghcr.io/uniget-org/tools/metadata:main --platform local --format raw-body \
 	| helper/usr/local/bin/jq --raw-output '.layers[0].digest' \
 	| xargs -I{} \
 		helper/usr/local/bin/regctl blob get ghcr.io/uniget-org/tools/metadata:main {} \
